d43230f5d59eaf38adaca6ae3f88bf0e9bbb042e
ChoiYoungWoo2197 <duddn2197@naver.com>
2019-12-06 18:10:10 +0900

    first commit

--- src/main/java/co/kr/controller/ShopController.java
+++ src/main/java/co/kr/controller/ShopController.java
@@ -1,9 +1,20 @@
 package co.kr.controller;
 
+import java.io.OutputStream;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +22,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,9 +32,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 @Controller
 public class ShopController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ShopController.class);
-	
+	List<Object> jsonlist =  new ArrayList<Object>();
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -31,21 +43,107 @@ public class ShopController {
 		try {
 			logger.info("shop call {}.", locale);
 			String authorityId = request.getParameter("authorityid");
-						
+
 			RestTemplate restTemplate = new RestTemplate();
 			ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://127.0.0.1:9220/controller/api/orders?authorityid="+authorityId, String.class);
 
 			ObjectMapper mapper = new ObjectMapper();
-			List<Object> jsonlist =  new ArrayList<Object>();
+
 			jsonlist = mapper.readValue(responseEntity.getBody(), new TypeReference<ArrayList<Object>>(){});
 
 			model.addAttribute("totalData", jsonlist);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		
-		
+
+
 		return "orders";
 	}
-	
+
+
+	@RequestMapping(value="/downLoadExcel.do")
+	public void downLoadExcel(HttpServletRequest request, HttpServletResponse response, Model model, @RequestParam("fileName") String fileName) throws Exception {
+		System.out.println(request.getRequestURI());
+		HSSFWorkbook objWorkBook = new HSSFWorkbook();
+		HSSFSheet objSheet = null;
+		HSSFRow objRow = null;
+		HSSFCell objCell = null;       //셀 생성
+
+		//제목 폰트
+		HSSFFont font = objWorkBook.createFont();
+		font.setFontHeightInPoints((short)9);
+		font.setBoldweight((short)font.BOLDWEIGHT_BOLD);
+		font.setFontName("맑은고딕");
+
+		//제목 스타일에 폰트 적용, 정렬
+		HSSFCellStyle styleHd = objWorkBook.createCellStyle();    //제목 스타일
+		styleHd.setFont(font);
+		styleHd.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+		styleHd.setVerticalAlignment (HSSFCellStyle.VERTICAL_CENTER);
+		objSheet = objWorkBook.createSheet("주문 현황 시트");     //워크시트 생성
+
+		// 1행 헤더 지정
+		String[] hearName = {"주문 식별번호","고객 식별번호","고객 이름","상품 식별번호","상품명"};
+		objRow = objSheet.createRow(0);
+		objRow.setHeight ((short) 0x150);
+
+		for (int i = 0; i < hearName.length; i++) {
+			objCell = objRow.createCell(i);
+			objCell.setCellValue(hearName[i]);
+			objCell.setCellStyle(styleHd);
+		}
+		
+		// 2행 값 입력
+		for (int i = 0; i < jsonlist.size(); i++) {
+			objRow = objSheet.createRow(i+1);
+			objRow.setHeight ((short) 0x150);
+
+			List<String> val = getInfos(jsonlist.get(i));
+
+			for (int i2 = 0; i2 < val.size(); i2++) {
+				objCell = objRow.createCell(i2);
+				objCell.setCellValue(val.get(i2));
+				objCell.setCellStyle(styleHd);
+			}
+		}
+
+
+		response.setContentType("Application/Msexcel");
+		response.setHeader("Content-Disposition", "ATTachment; Filename="+URLEncoder.encode(fileName,"UTF-8")+".xls");
+
+		OutputStream fileOut  = response.getOutputStream();
+		objWorkBook.write(fileOut);
+		fileOut.close();
+
+		response.getOutputStream().flush();
+		response.getOutputStream().close();
+	}
+
+	public List<String> getInfos(Object object) {
+		List<String> data = new ArrayList<String>();
+		
+		String result = object.toString().trim();
+		result = result.replace("{", "");
+		result = result.replace("}", "");
+
+		for (int i = 0; i < result.length(); i++) {
+			if(result.charAt(i) == '=') {
+				String str = "";
+				for(int i2 = i+1; i2 < result.length(); i2++) {
+					if(result.charAt(i2) != ',') {
+						str += result.charAt(i2);
+					}
+					else {
+						break;
+					}
+				}
+				data.add(str);
+				
+			}
+		}
+
+		return data;
+	}
+
+
 }
--- target/controller-1.0.0-BUILD-SNAPSHOT/WEB-INF/views/orders.jsp
+++ target/controller-1.0.0-BUILD-SNAPSHOT/WEB-INF/views/orders.jsp
@@ -1,33 +1,38 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false"%>
 <html>
 <head>
-	<title>cmt_spring_level_check</title>
-	<link rel="stylesheet" href="/resources/css/style.css" />
+<title>cmt_spring_level_check</title>
+<link rel="stylesheet" href="./resources/css/style.css" />
+
 </head>
 <body>
-<h1>
-	주문 현항 테이블
-</h1>
+	<h1>주문 현항 테이블</h1>
 
-<table class= "bluetop">
- <tr>
-    <th>주문 식별번호</th>
-    <th>고객 식별번호</th>
-    <th>고객 이름</th>
-    <th>상품 식별번호</th>
-    <th>상품명</th>
-  </tr>    
-<c:forEach var="data" items="${totalData}">
-  <tr>
-    <th>${data.orderId}</th>
-    <th>${data.customId}</th>
-    <th>${data.customName}</th>
-    <th>${data.productId}</th>
-    <th>${data.productName}</th>
-  </tr>    
-</c:forEach>
-</table>
+	<table class="bluetop">
+		<tr>
+			<th>주문 식별번호</th>
+			<th>고객 식별번호</th>
+			<th>고객 이름</th>
+			<th>상품 식별번호</th>
+			<th>상품명</th>
+		</tr>
+		<c:forEach var="data" items="${totalData}">
+			<tr>
+				<th>${data.orderId}</th>
+				<th>${data.customId}</th>
+				<th>${data.customName}</th>
+				<th>${data.productId}</th>
+				<th>${data.productName}</th>
+			</tr>
+		</c:forEach>
+	</table>
+
+	<form id="excelForm" name="excelForm" method="post"	action="downLoadExcel.do">
+		<input type="text" name="fileName" />
+		<input type="submit" value="엑셀 파일로 받기" />
+	</form>
 </body>
 </html>
